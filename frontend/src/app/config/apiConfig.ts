75d88a68142d4475ea1899533e0746b1566daecd
vaajiee <vajidmohammed94@gmail.com>
2023-08-30 17:47:21 +0530

    prod: updated favicon, api baseUrl

--- frontend/src/app/config/apiConfig.ts
+++ frontend/src/app/config/apiConfig.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/v1";
+const BASE_URL = "https://commonimagestore.abdulvajid.com/api/v1";
 
 const axiosPublic = axios.create({
     baseURL: BASE_URL,
